2b9d8854dd8c05de742c66b4964401371b3a5689
Kevin Moens <kevmoens@yahoo.com>
2022-02-01 21:16:54 -0600

    Create some Appointment data entry form

--- Wuphf/Shared/Appointment.cs
+++ Wuphf/Shared/Appointment.cs
@@ -14,8 +14,9 @@ namespace Wuphf.Shared
         public string Description { get; set; }
         public DateTime? ScheduleTime { get; set; }
         public DateTime? EndDate { get; set; }
-        private ReoccuranceTypes reoccuranceTypes = ReoccuranceTypes.None;
-        public ReoccuranceTypes Reoccurance { get => reoccuranceTypes; set { reoccuranceTypes = value; } }
+        private ReoccuranceTypes? reoccuranceTypes = ReoccuranceTypes.None;
+        [EnumDataType(typeof(ReoccuranceTypes))]
+        public ReoccuranceTypes? Reoccurance { get => reoccuranceTypes; set { reoccuranceTypes = value; } }
         public int? NumDaysBetween { get; set; }
         public int? WeekDays { get; set; }
         public bool? SkipWeekend { get; set; }
