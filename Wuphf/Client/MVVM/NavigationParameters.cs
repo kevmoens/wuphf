951938cfae99cab1d1018729dce0817bc8c8e6b1
Kevin Moens <kevmoens@yahoo.com>
2022-04-13 20:28:19 -0500

    Create RegionService for Blazor (MVVM)

---
+++ Wuphf/Client/MVVM/NavigationParameters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wuphf.Client.MVVM
+{
+    public class NavigationParameters 
+    {
+        Dictionary<string,Dictionary<string, object>> Parameters { get; set; } = new Dictionary<string, Dictionary<string, object>>();
+    }
+}
